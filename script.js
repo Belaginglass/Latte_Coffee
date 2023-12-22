2b39ccfb3931a7e84270f25e229d3008e532a8e6
Isabela Ginglass <gaabiscuit@gmail.com>
2023-12-22 00:18:23 -0300

    Update
    Novas sub Páginas

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('home').classList.add('selected');
 });
+
+function showRegisterForm() {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('registerForm').style.display = 'block';
+  }
+
+  function showLoginForm() {
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('registerForm').style.display = 'none';
+  }
+
+  function goToHomePage() {
+    window.location.href = 'index.html';
+  }
+
